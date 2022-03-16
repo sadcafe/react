0c296b1f2c57ffc43ed02798ae1c7d5b74519a70
sadcafe <42790600+sadcafe@users.noreply.github.com>
2022-03-16 17:28:41 +0900

    study

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,24 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Fragment } from 'react';
+import './App.css';
 
 function App() {
+  const name = '테스트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div>{name || '영희'}</div>
+      <div>Test...</div>
+    </Fragment>
   );
 }
 
