c6f002562bfe8a901e7e125203513a959aa5d5cb
sadcafe <42790600+sadcafe@users.noreply.github.com>
2022-03-29 17:42:01 +0900

    chapter 08 study

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,51 @@
+// import Counter from "./Counter";
+// import { useState } from "react";
+// import Info from "./Info";
+import Average from "./Average";
+
+const App = () => {
+  return <Average />;
+};
+
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button onClick={() => setVisible(!visible)}>
+//         {visible ? "숨기기" : "보이기"}
+//       </button>
+//       <hr />
+//       {visible && <Info></Info>}
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return <Counter></Counter>;
+// };
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ hooks-tutorial/src/Info.js
@@ -0,0 +1,40 @@
+// import { useState, useEffect, useReducer } from "react";
+// import { useReducer } from "react";
+import useInputs from "./useInputs";
+
+// function reducer(state, action) {
+//   return {
+//     ...state,
+//     [action.name]: action.value,
+//   };
+// }
+
+const Info = () => {
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+
+  const { name, nickname } = state;
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange}></input>
+        <input name="nickname" value={nickname} onChange={onChange}></input>
+      </div>
+      <div>
+        <div>
+          <b>이름 : </b>
+          {name}
+        </div>
+        <div>
+          <b>닉네임 : </b>
+          {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
