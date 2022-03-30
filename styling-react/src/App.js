1a4593414096526db4e51d49cd4d713ea542d8c9
sadcafe <42790600+sadcafe@users.noreply.github.com>
2022-03-30 13:45:36 +0900

    chapter09 study

---
+++ styling-react/src/App.js
@@ -0,0 +1,32 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+import { Component } from "react";
+import SassComponent from "./SassComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent></SassComponent>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <img src={logo} className="logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
